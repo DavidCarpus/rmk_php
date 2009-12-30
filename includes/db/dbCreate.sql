0cc3d6b4d8ac034534032bede07b5952f04c3452
David Carpus <David.Carpus@gmail.com>
2009-12-30 10:28:31 -0500

    Change from 'rmk' db to 'newrmk' db

--- includes/db/dbCreate.sql
+++ includes/db/dbCreate.sql
@@ -1,6 +1,6 @@
-drop table  if exists `rmk`.`users`;
-CREATE TABLE `rmk`.`users` (
-  `user_id` INT(11)  DEFAULT 0 AUTO_INCREMENT NOT NULL,
+drop table  if exists `newrmk`.`users`;
+CREATE TABLE `newrmk`.`users` (
+  `user_id` INT(11)  AUTO_INCREMENT NOT NULL,
   `user` VARCHAR(80)  NOT NULL,
   `email` VARCHAR(80)  NOT NULL,
   `password` VARCHAR(20)  NOT NULL,
@@ -8,22 +8,22 @@ CREATE TABLE `rmk`.`users` (
   PRIMARY KEY(`user_id`)
 );
 
-insert into `rmk`.`users` set 
+insert into `newrmk`.`users` set 
 	user="dcarpus", email="csdave2000@yahoo.com", password="";
 	
 
-drop table  if exists `rmk`.`catalogcategories`;
-CREATE TABLE `rmk`.`catalogcategories` (
-  `catalogcategories_id` INT(11)  DEFAULT 0 AUTO_INCREMENT NOT NULL,
+drop table  if exists `newrmk`.`catalogcategories`;
+CREATE TABLE `newrmk`.`catalogcategories` (
+  `catalogcategories_id` INT(11)  AUTO_INCREMENT NOT NULL,
   `category` VARCHAR(120)  NOT NULL,
   `tag` VARCHAR(20)  NOT NULL,
   `active` TINYINT  DEFAULT 1 NOT NULL,
   PRIMARY KEY(`catalogcategories_id`)
 );
 
-drop table  if exists `rmk`.`knifephotos`;
-CREATE TABLE `rmk`.`knifephotos` (
-  `photo_id` INT(11)  DEFAULT 0 AUTO_INCREMENT NOT NULL,
+drop table  if exists `newrmk`.`knifephotos`;
+CREATE TABLE `newrmk`.`knifephotos` (
+  `photo_id` INT(11) AUTO_INCREMENT NOT NULL,
   `filelocation` VARCHAR(120)  NOT NULL,
   `photo_labels` VARCHAR(80)  NOT NULL,
   `catalogcategories_id` INT(11)  DEFAULT 0 NOT NULL,
@@ -31,21 +31,21 @@ CREATE TABLE `rmk`.`knifephotos` (
   PRIMARY KEY(`photo_id`)
 );
 
-insert into `rmk`.`catalogcategories` set category="Outdoorsman Knives", tag="outdoorsman";
-insert into `rmk`.`catalogcategories` set category="Skinning and Hunting Knives", tag="hunting";
-insert into `rmk`.`catalogcategories` set category="Saltwater Knives", tag="saltwater";
-insert into `rmk`.`catalogcategories` set category="Survival Knives", tag="survival";
-insert into `rmk`.`catalogcategories` set category="Military Style Knives", tag="military";
-insert into `rmk`.`catalogcategories` set category="Bowie Knives", tag="bowie";
-insert into `rmk`.`catalogcategories` set category="Carving Set & Steak Knife", tag="carving";
-insert into `rmk`.`catalogcategories` set category="Sheaths", tag="sheaths";
-insert into `rmk`.`catalogcategories` set category="Non-Catalog II Knives", tag="noncatalog";
-insert into `rmk`.`catalogcategories` set category="Example Combinations", tag="examples";
+insert into `newrmk`.`catalogcategories` set category="Outdoorsman Knives", tag="outdoorsman";
+insert into `newrmk`.`catalogcategories` set category="Skinning and Hunting Knives", tag="hunting";
+insert into `newrmk`.`catalogcategories` set category="Saltwater Knives", tag="saltwater";
+insert into `newrmk`.`catalogcategories` set category="Survival Knives", tag="survival";
+insert into `newrmk`.`catalogcategories` set category="Military Style Knives", tag="military";
+insert into `newrmk`.`catalogcategories` set category="Bowie Knives", tag="bowie";
+insert into `newrmk`.`catalogcategories` set category="Carving Set & Steak Knife", tag="carving";
+insert into `newrmk`.`catalogcategories` set category="Sheaths", tag="sheaths";
+insert into `newrmk`.`catalogcategories` set category="Non-Catalog II Knives", tag="noncatalog";
+insert into `newrmk`.`catalogcategories` set category="Example Combinations", tag="examples";
 
 
-drop table  if exists `rmk`.`knifemodels`;
-CREATE TABLE `rmk`.`knifemodels` (
-  `knifemodels_id` INT(11)  DEFAULT 0 AUTO_INCREMENT NOT NULL,
+drop table  if exists `newrmk`.`knifemodels`;
+CREATE TABLE `newrmk`.`knifemodels` (
+  `knifemodels_id` INT(11)  AUTO_INCREMENT NOT NULL,
   `catalogcategories_id` INT(11)  DEFAULT 0 NOT NULL,
   `model` VARCHAR(120)  NOT NULL,
   `piclabel` CHAR(2)  NOT NULL,
@@ -58,9 +58,9 @@ CREATE TABLE `rmk`.`knifemodels` (
 );
 
 
-drop table  if exists `rmk`.`orders`;
-CREATE TABLE `rmk`.`orders` (
-  `orders_id` INT(11)  DEFAULT 0 AUTO_INCREMENT NOT NULL,
+drop table  if exists `newrmk`.`orders`;
+CREATE TABLE `newrmk`.`orders` (
+  `orders_id` INT(11) AUTO_INCREMENT NOT NULL,
   `processed` TINYINT  DEFAULT 0 NOT NULL,
   `name` VARCHAR(120)  NOT NULL,
   `email` VARCHAR(120)  NOT NULL,
@@ -91,9 +91,9 @@ CREATE TABLE `rmk`.`orders` (
 );
 
 
-drop table  if exists `rmk`.`faq`;
-CREATE TABLE `rmk`.`faq` (
-  `faq_id` INT(11)  DEFAULT 0 AUTO_INCREMENT NOT NULL,
+drop table  if exists `newrmk`.`faq`;
+CREATE TABLE `newrmk`.`faq` (
+  `faq_id` INT(11)  AUTO_INCREMENT NOT NULL,
   `question` TEXT NOT NULL,
   `answer` TEXT NOT NULL,
   `active` TINYINT  DEFAULT 1 NOT NULL,
@@ -101,9 +101,9 @@ CREATE TABLE `rmk`.`faq` (
 );
 
 
-drop table  if exists `rmk`.`webcopy`;
-CREATE TABLE `rmk`.`webcopy` (
-  `webcopy_id` INT(11)  DEFAULT 0 AUTO_INCREMENT NOT NULL,
+drop table  if exists `newrmk`.`webcopy`;
+CREATE TABLE `newrmk`.`webcopy` (
+  `webcopy_id` INT(11) AUTO_INCREMENT NOT NULL,
   `page` VARCHAR(60) NOT NULL,
   `prefix` TEXT NOT NULL,
   `postfix` TEXT NOT NULL,
@@ -111,9 +111,9 @@ CREATE TABLE `rmk`.`webcopy` (
 );
 
 
-drop table  if exists `uplzcvgw_rmk`.`emails`;
-CREATE TABLE `uplzcvgw_rmk`.`emails` (
-  `email_id` INT(11)  DEFAULT 0 AUTO_INCREMENT NOT NULL,
+drop table  if exists `newrmk`.`emails`;
+CREATE TABLE `newrmk`.`emails` (
+  `email_id` INT(11)  AUTO_INCREMENT NOT NULL,
   `fromaddress` VARCHAR(60) NOT NULL,
   `toaddress` VARCHAR(60) NOT NULL,
   `messagesubject` TEXT NOT NULL,
@@ -121,7 +121,7 @@ CREATE TABLE `uplzcvgw_rmk`.`emails` (
   PRIMARY KEY(`email_id`)
 );
 
-
+/*
 --ALTER TABLE `orders` ADD `ccvcode` VARCHAR(6) AFTER `ccnumber`;
 --ALTER TABLE `orders` ADD `datesubmitted` TIMESTAMP AFTER `ccname`;
 --ALTER TABLE `orders` ADD `address3` VARCHAR(120) AFTER `address2`;
@@ -130,8 +130,9 @@ CREATE TABLE `uplzcvgw_rmk`.`emails` (
 --ALTER TABLE `knifemodels` change `price` `price` VARCHAR(120)  NOT NULL;
 
 --ALTER TABLE `orders` ADD `comment` TEXT AFTER `processed`
+*/
 
-insert into `rmk`.`webcopy` set 
+insert into `newrmk`.`webcopy` set 
 page = "catalog",
 prefix="All current catalog and non-catalog information with price lists will be sent via airmail.
 There is a charge for catalogs mailed outside the United States.
@@ -141,26 +142,28 @@ All Other Countries - US$5.00
 Please use this Secure Form, or if you prefer, print this form and fax it to us (407) 855-9054. Minimum age to order is 16 years old", 
 postfix="";
 
-insert into `rmk`.`webcopy` set 
+insert into `newrmk`.`webcopy` set 
 page = "knifecare",
 prefix="Fixing/cleaning a knife", 
 postfix="";
 
-insert into `rmk`.`webcopy` set 
+insert into `newrmk`.`webcopy` set 
 page = "faq",
 prefix="Frequently Asked Questions", 
 postfix="";
 
-insert into `rmk`.`webcopy` set 
+insert into `newrmk`.`webcopy` set 
 page = "order",
 prefix="Current order deliveries are being scheduled in approximately 54 MONTHS. Order limit is two knives every other month per household. We strongly recommend obtaining the catalog to view all features before placing an order. A deposit of US$50.00 per knife is required to place an order. Deposits are not transferable and non-refundable. Credit card user name must match the individual’s name placing the order. Shipping charges are determined by Randall Made Knives based on weight, value and destination in the year of delivery. Mimimum age to order is 16 years old. EFFECTIVE MARCH 1, 2006, RANDALL WILL LIMIT ORDERS TO A SINGLE KNIFE EVERY THREE MONTHS PER HOUSEHOLD.", 
 postfix="";
 
 
-mysql> create index CustID on Customers (CustomerID);
-mysql> create index KnifeInv on InvoiceEntries (Invoice);
+
 
 /*
+create index CustID on Customers (CustomerID);
+create index KnifeInv on InvoiceEntries (Invoice);
+
 update orders set processed=0 where email='csdave2000@yahoo.com';
 select * from orders where email='csdave2000@yahoo.com';
 
