d991629cd17d3afc1343dd5c7d626f027231839a
David Carpus <David.Carpus@gmail.com>
2010-03-06 12:44:49 -0800

    Rework to allow disconnected laptop

--- config.php
+++ config.php
@@ -1,40 +1,43 @@
 <?php
+$system="";
+
+foreach (array('192.168.1.99', '192.168.1.90', 'carpus.homelinux.org') as $address) {
+	if(substr($_SERVER['SERVER_ADDR'] ,0,strlen($address)) == $address ){
+		$system="CARPUS_SERVER";
+		break;
+	}
+}
+foreach (array('192.168.1.3', '127.0.0.1', 'localhost') as $address) {
+	if(substr($_SERVER['SERVER_ADDR'] ,0,strlen($address)) == $address ){
+		$system="CARPUS_LAPTOP";
+		break;
+	}
+	break;
+}
+foreach (array('192.168.1.110') as $address) {
+	if(substr($_SERVER['SERVER_ADDR'] ,0,strlen($address)) == $address ){
+		$system="SHOP_SERVER";
+	}
+	break;
+}
+if($system=="" && ($_SERVER['HTTP_HOST'] == 'localhost')){
+	$system="CARPUS_LAPTOP";
+}
+
+
 function isCarpusServer(){
-	$address = '192.168.1.99';
-	if(substr($_SERVER['SERVER_ADDR'] ,0,strlen($address)) == $address ){
-		return true;
-	}
-	$address = '192.168.1.90';
-	if(substr($_SERVER['SERVER_ADDR'] ,0,strlen($address)) == $address ){
-		return true;
-	}
-	$address = 'carpus.homelinux.org';
-	if(substr($_SERVER['SERVER_ADDR'] ,0,strlen($address)) == $address ){
-		return true;
-	}
-	return false;
+	global $system;
+	return $system=="CARPUS_SERVER";
 }
 function isCarpusLaptop(){
-	$address = '192.168.1.3';
-	if(substr($_SERVER['SERVER_ADDR'] ,0,strlen($address)) == $address ){
-		return true;
+	global $system;
+	return $system=="CARPUS_LAPTOP";
 	}
-	$address = '127.0.0.1';
-	if(substr($_SERVER['SERVER_ADDR'] ,0,strlen($address)) == $address ){
-		return true;
-	}
-	$address = 'localhost';
-	if(substr($_SERVER['SERVER_ADDR'] ,0,strlen($address)) == $address ){
-		return true;
-	}
-}
 function isShopServer(){
-	$address = '192.168.1.110';
-	if(substr($_SERVER['SERVER_ADDR'] ,0,strlen($address)) == $address ){
-		return true;
-	}
-	return false;
+	global $system;
+	return $system=="SHOP_SERVER";
 }
+//echo $system;
 if(isShopServer()){
 define("INCLUDE_DIR", "/var/www/html/test/includes/");
 define("FORMS_DIR", "/var/www/html/test/includes/forms/");
@@ -48,7 +51,7 @@ define("DB_INC_DIR", "/var/www/rmk/includes/db/");
 define("PDF_FONT_DIR", "/var/www/rmk/includes/pdfCreator/fonts/");
 	
 } else{
-	echo "Unknown system config: " . $_SERVER['SERVER_ADDR'];
+	echo "Unknown system config: " . $_SERVER['SERVER_ADDR'] . ":" . $_SERVER['HTTP_HOST'];
 }
 
 define("MAX_EMAIL_LIST_LEN", 90);
