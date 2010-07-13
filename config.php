7c9b5eb8ef0dfb6109f25d4b57b11b60e31087be
David Carpus <David.Carpus@gmail.com>
2010-07-12 20:06:05 -0400

    Make catalog images work on localhost.

--- config.php
+++ config.php
@@ -44,19 +44,20 @@ define("INCLUDE_DIR", "/var/www/html/test/includes/");
 define("FORMS_DIR", "/var/www/html/test/includes/forms/");
 define("DB_INC_DIR", "/var/www/html/test/includes/db/");
 define("PDF_FONT_DIR", "/var/www/html/test/includes/pdfCreator/fonts/");
+define("BASE_IMG_URL", "/test/images/");
 
 } else if(isCarpusServer() || isCarpusLaptop() ){
 define("INCLUDE_DIR", "/var/www/rmk/includes/");
 define("FORMS_DIR", "/var/www/rmk/includes/forms/");
 define("DB_INC_DIR", "/var/www/rmk/includes/db/");
 define("PDF_FONT_DIR", "/var/www/rmk/includes/pdfCreator/fonts/");
-	
+define("BASE_IMG_URL", "/rmk/images/");
+
 } else{
 	echo "Unknown system config: " . $_SERVER['SERVER_ADDR'] . ":" . $_SERVER['HTTP_HOST'];
 	exit;
 }
 
 define("MAX_EMAIL_LIST_LEN", 90);
-define("BASE_IMG_URL", "/test/images/");
 define("BASE_IMG_FILEPATH", "/var/www/html/test/images/catalog");
 ?>
